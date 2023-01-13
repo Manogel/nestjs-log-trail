69bfa79d51f846f4a70ad8f67b27992d3dc68056
Manogel <manoelgomes53@gmail.com>
2023-01-13 00:22:20 -0300

    feat: create index module

---
+++ src/app-logger/index.ts
@@ -0,0 +1,6 @@
+export { AppLoggerModule } from './appLogger.module';
+export { AppLoggerService } from './appLogger.service';
+export { AppLoggerParams } from './types/AppLoggerParams.type';
+export { AppLoggerObj } from './types/AppLoggerObj.type';
+export { AppLoggerRequest } from './types/AppLoggerRequest.type';
+export { AppLoggerResponse } from './types/AppLoggerResponse.type';

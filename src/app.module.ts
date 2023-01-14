973548328c390f54d9c19442112eaef3649a8ebe
Manogel <manoelgomes53@gmail.com>
2023-01-14 17:41:40 -0300

    fix: package files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppLoggerModule } from './app-logger/appLogger.module';
+// import { AppLoggerModule } from './app-logger/appLogger.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AppLoggerModule } from '@manogel/nestjs-log-trail';
 
 @Module({
   imports: [
